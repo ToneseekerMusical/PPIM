e29a09bac1be84495a5773121be1ce70fae86fdc
ToneseekerMusical <105748910+ToneseekerMusical@users.noreply.github.com>
2023-02-03 17:46:52 -0500

    Refactored various functions

---
+++ DatabaseControl/MongoState.py
@@ -0,0 +1,33 @@
+import pathlib
+from subprocess import Popen, DETACHED_PROCESS, run
+
+def selectFile(path,file):
+  path = path.iterdir()
+  path = list(filter(lambda path: path.is_dir(), path))
+  path = [x for x in path if 'mongo' in x.name][0].iterdir()
+  path = list(filter(lambda path: path.is_dir(), path))[0].iterdir()
+  bin = [x for x in path if file in x.name.lower()]
+  return bin
+
+def startMongoDB():
+    path = pathlib.Path(__file__).parent.parent
+    srvPath = path.joinpath(str(path)+'/srv/mongodb')
+    if srvPath.is_dir() == False:
+      srvPath.mkdir(parents=True)
+    print(srvPath)
+
+    Popen([
+      "mongod.exe",
+      "--dbpath", str(srvPath),
+    ], creationflags=DETACHED_PROCESS)
+
+def stopMongoDB():
+   Popen([
+      "mongod.exe",
+      "--shutdown"
+   ])
+
+def startCompass():
+   run('MongoDBCompass')
+
+startMongoDB()
--- Installs/installMongoDB.py
+++ Installs/installMongoDB.py
@@ -2,31 +2,41 @@ import pathlib
 import os
 import re
 import ctypes
-from subprocess import Popen, CREATE_NEW_CONSOLE
+from subprocess import Popen
+from shutil import which
 
 def selectFile(file):
-    path = pathlib.Path(__file__).parent.iterdir()
-    dirs = list(filter(lambda path: path.is_dir(), path))
-    mongoDir = [x for x in dirs if 'mongo' in x.name][0].iterdir()
-    mongoBin = list(filter(lambda path: path.is_dir(), mongoDir))[0].iterdir()
-    bin = [x for x in mongoBin if file in x.name.lower()]
-    return bin
+  path = pathlib.Path(__file__).parent.iterdir()
+  dirs = list(filter(lambda path: path.is_dir(), path))
+  mongoDir = [x for x in dirs if 'mongo' in x.name][0].iterdir()
+  mongoBin = list(filter(lambda path: path.is_dir(), mongoDir))[0].iterdir()
+  bin = [x for x in mongoBin if file in x.name.lower()]
+  return bin
 
-def setPATH(path):
+def checkPATH(path, env):
   p = str(path)[0].upper() + str(path)[1:]
-  env=os.getenv('PATH')
-  env=env.split(';')
-  env = [x for x in env if x != '']
-  env = [sub.replace('WINDOWS', '%SystemRoot%') for sub in env]
-  env = [(re.sub(r'(.*)\\Users\\(.*)\\AppData',"%USERPROFILE%\\\\AppData",x)) for x in env]
+  if p in env:
+    return True
+  else:
+    return False
 
+def setPath(path, env):
+  p = str(path)[0].upper() + str(path)[1:]
+
+  #remove empty values
+  env = [x for x in env if x != '']
+
+  #Replace absolute paths prefixes with wildcards
+  env = [sub.replace('C:\WINDOWS', '%SystemRoot%') for sub in env]
+  env = [(re.sub(r'(.*)\\Users\\(.*)\\AppData',"%USERPROFILE%\\\\AppData",x)) for x in env]
+  
   if p not in env:
-    print(p)
     env.append(p)
     env = ';'.join(env)
     var = "PATH"
     scope = "Machine"
     commands = u"[Environment]::SetEnvironmentVariable('"+var+"','"+env+"','"+scope+"')"
+    
     ctypes.windll.shell32.ShellExecuteW(
       None,
       u"runas",
@@ -35,35 +45,30 @@ def setPATH(path):
       None,
       1
     )
+  print('Set Path Ran!')
+  return env
+
+def installCompass():
+  file = 'compass'
+  file = selectFile(file)[0]
+  Popen([
+    "powershell.exe",
+    str(file)
+  ])
         
+def setupMongoDB():
+  file = 'mongo'
+  file = selectFile(file)[0]
+  folder = file.parent
+  cpath = '%USERPROFILE%\AppData\Local\MongoDBCompass'
+  env = str(os.getenv('PATH')).split(';')
+  compass = which('MongoDBCompass')
+  
+  if checkPATH(folder, env) == False:
+    env = setPath(folder, env)
+  
+  if compass is None:
+    installCompass()
+    setPath(cpath, env)
 
-def startMongos():
-    file = 'mongos.exe'
-    file = selectFile(file)[0]
-    folder = file.parent
-    setPATH(folder)
-    Popen([
-      "mongos.exe",
-    ], creationflags=CREATE_NEW_CONSOLE)
-
-def startMongod():
-    path = pathlib.Path()
-    if path.is_dir():
-       path.joinpath('/srv/mongodb')
-    else:
-       path.mkdir(parents=True)
-
-    print(path)
-       
-    file = 'mongod.exe'
-    file = selectFile(file)[0]
-    folder = file.parent
-    setPATH(folder)
-    Popen([
-      "mongod.exe",
-      "--dbpath", str(path),
-      "--port", "27019"
-    ], creationflags=CREATE_NEW_CONSOLE)
-
-startMongod()
-startMongos()
+setupMongoDB()
