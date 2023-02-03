0a395e6814a56a76343cdeb4ab908b1af37d7437
ToneseekerMusical <105748910+ToneseekerMusical@users.noreply.github.com>
2023-02-03 06:58:34 -0500

    Mongod start script

--- Installs/installMongoDB.py
+++ Installs/installMongoDB.py
@@ -1,7 +1,8 @@
 import pathlib
 import os
 import re
-import subprocess
+import ctypes
+from subprocess import Popen, CREATE_NEW_CONSOLE
 
 def selectFile(file):
     path = pathlib.Path(__file__).parent.iterdir()
@@ -11,35 +12,58 @@ def selectFile(file):
     bin = [x for x in mongoBin if file in x.name.lower()]
     return bin
 
-def checkPATH(path):
-    env=os.getenv('PATH')
-    env=env.split(';')
-    env = [x for x in env if x != '']
-    env = [sub.replace('WINDOWS', '%SystemRoot%') for sub in env]
-    env = [(re.sub(r'(.*)\\Users\\(.*)\\AppData',"%USERPROFILE%\\\\AppData",x)) for x in env]
+def setPATH(path):
+  p = str(path)[0].upper() + str(path)[1:]
+  env=os.getenv('PATH')
+  env=env.split(';')
+  env = [x for x in env if x != '']
+  env = [sub.replace('WINDOWS', '%SystemRoot%') for sub in env]
+  env = [(re.sub(r'(.*)\\Users\\(.*)\\AppData',"%USERPROFILE%\\\\AppData",x)) for x in env]
 
-
-    if path in env:
-        print('Path is already in environment')
-    else:
-        env.append(str(path).capitalize())
-        env = ';'.join(env)
-        envCMD = "[Environment]::SetEnvironmentVariable('PATH',"+env+"'Machine')"
-        print(envCMD)
-#        subprocess.Popen('powershell.exe')
+  if p not in env:
+    print(p)
+    env.append(p)
+    env = ';'.join(env)
+    var = "PATH"
+    scope = "Machine"
+    commands = u"[Environment]::SetEnvironmentVariable('"+var+"','"+env+"','"+scope+"')"
+    ctypes.windll.shell32.ShellExecuteW(
+      None,
+      u"runas",
+      u"powershell.exe",
+      commands,
+      None,
+      1
+    )
         
 
-def start(file):
+def startMongos():
+    file = 'mongos.exe'
     file = selectFile(file)[0]
     folder = file.parent
-    checkPATH(folder)
-    #os.chdir(str(folder))
-    #os.system(file.name)
+    setPATH(folder)
+    Popen([
+      "mongos.exe",
+    ], creationflags=CREATE_NEW_CONSOLE)
 
-start('mongos.exe')
+def startMongod():
+    path = pathlib.Path()
+    if path.is_dir():
+       path.joinpath('/srv/mongodb')
+    else:
+       path.mkdir(parents=True)
 
-#Mongo('mongos.exe')
-#Mongo('mongod.exe')
+    print(path)
+       
+    file = 'mongod.exe'
+    file = selectFile(file)[0]
+    folder = file.parent
+    setPATH(folder)
+    Popen([
+      "mongod.exe",
+      "--dbpath", str(path),
+      "--port", "27019"
+    ], creationflags=CREATE_NEW_CONSOLE)
 
-#file = selectFile('mongod.exe')[0]
-#file = selectFile('mongos.exe')[0]
+startMongod()
+startMongos()
