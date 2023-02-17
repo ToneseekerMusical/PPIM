1c9d12e6e5dc3065417e1d71fa16708a2de98d81
ToneseekerMusical <105748910+ToneseekerMusical@users.noreply.github.com>
2023-02-16 22:50:12 -0500

    Swaps in a stop button when NodeJS is initiated.

--- GUI/Frames/ProjectFrames/DevTools.py
+++ GUI/Frames/ProjectFrames/DevTools.py
@@ -12,30 +12,38 @@ class devTools(ctk.CTkTabview):
       *args,
       **kwargs)
     #create shell frame
+    self.frontend = frontend
     self.add('Dev Tools')
     self.tab('Dev Tools').grid_columnconfigure((0,1),weight=1)
-    self.startFrontend = ctk.CTkButton(self.tab('Dev Tools'),text='Start Frontend',command=threading.Thread(target=lambda:self.startFront(frontend['frontendPath'])).start)
+    self.startFrontend = ctk.CTkButton(self.tab('Dev Tools'),text='Start Frontend',command=threading.Thread(target=lambda:self.startFront(self.frontend['frontendPath']),daemon=True).start)
     self.startFrontend.grid(row=0,column=0,padx=5,pady=5,sticky='ew')
-    self.buildFrontend = ctk.CTkButton(self.tab('Dev Tools'),text='Build Frontend',command=threading.Thread(target=lambda:self.buildSite(frontend['frontendPath'])).start)
+    self.buildFrontend = ctk.CTkButton(self.tab('Dev Tools'),text='Build Frontend',command=threading.Thread(target=lambda:self.buildSite(self.frontend['frontendPath'])).start)
     self.buildFrontend.grid(row=0,column=1,padx=5,pady=5,sticky='ew')
-    self.startAdmin = ctk.CTkButton(self.tab('Dev Tools'),text='Start Admin',command=threading.Thread(target=lambda:self.AdminStart(frontend['adminPath'])).start)
+    self.startAdmin = ctk.CTkButton(self.tab('Dev Tools'),text='Start Admin',command=threading.Thread(target=lambda:self.AdminStart(self.frontend['adminPath'])).start)
     self.startAdmin.grid(row=1,column=0,padx=5,pady=5,sticky='ew')
-    self.buildAdmin = ctk.CTkButton(self.tab('Dev Tools'),text='Build Admin',command=threading.Thread(target=lambda:self.buildSite(frontend['adminPath'])).start)
+    self.buildAdmin = ctk.CTkButton(self.tab('Dev Tools'),text='Build Admin',command=threading.Thread(target=lambda:self.buildSite(self.frontend['adminPath'])).start)
     self.buildAdmin.grid(row=1,column=1,padx=5,pady=5,sticky='ew')
 
   def buildSite(self,siteDir):
     subprocess.Popen(['powershell','npm','run','build'],cwd=siteDir)
 
+  
+  def stopFront(self):
+    self.front.terminate()
+    self.startFrontend = ctk.CTkButton(self.tab('Dev Tools'),text='Start Frontend',command=threading.Thread(target=lambda:self.startFront(self.frontend['frontendPath']),daemon=True).start)
+    self.startFrontend.grid(row=0,column=0,padx=5,pady=5,sticky='ew')
+
+  def AdminStop(self):
+    self.admin.kill()
+    self.startAdmin = ctk.CTkButton(self.tab('Dev Tools'),text='Start Admin',command=threading.Thread(target=lambda:self.AdminStart(self.frontend['adminPath']),).start)
+    self.startAdmin.grid(row=1,column=0,padx=5,pady=5,sticky='ew')
+  
   def startFront(self,siteDir):
     self.front = subprocess.Popen(['powershell','npm','run','dev'],cwd=siteDir)
-    self.stopFrontend = ctk.CTkButton(self.tab('Dev Tools'),text='Stop Frontend')
+    self.stopFrontend = ctk.CTkButton(self.tab('Dev Tools'),text='Stop Frontend',command=self.stopFront)
     self.stopFrontend.grid(row=0,column=0,padx=5,pady=5,sticky='ew')
 
   def AdminStart(self,siteDir):
     self.admin = subprocess.Popen(['powershell','npm','run','dev'],cwd=siteDir)
-    self.stopAdmin = ctk.CTkButton(self.tab('Dev Tools'),text='Stop Admin')
+    self.stopAdmin = ctk.CTkButton(self.tab('Dev Tools'),text='Stop Admin', command=self.AdminStop)
     self.stopAdmin.grid(row=1,column=0,padx=5,pady=5,sticky='ew')
-  
-  def stopFront(self):...
-
-  def AdminStop(self):...
